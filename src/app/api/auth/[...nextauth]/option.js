76feb3e9c7a626d5aa1ec3c045f68291d306348d
Dickson <=>
2024-10-27 23:50:26 +0800

    update

--- src/app/api/auth/[...nextauth]/option.js
+++ src/app/api/auth/[...nextauth]/option.js
@@ -33,9 +33,11 @@ export const options = {
   ],
   callbacks: {
     async redirect({ url, baseUrl }) {
+      return url;
+
       // Checks if URL is an internal URL or external URL
       if (url.startsWith("/")) return `${baseUrl}${url}`; // Redirect to internal pages
-      return baseUrl; // Default to base URL if nothing specified
+      return `${baseUrl}/admin`; // Default to base URL if nothing specified
     },
   },
 };
